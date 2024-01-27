e4617f33a2daa4bea8e626cb4fcf21874f3da080
Darsh Kejriwal <c-darsh@aulacube.com>
2024-01-27 23:05:24 +0530

    file name

--- utils/usersocketmap.js
+++ utils/usersocketmap.js
@@ -1,22 +1,22 @@
 // UserSocketMap.js
 
 class SocketMap {
-	constructor() {
+	constructor () {
 		this.userSockets = new Map();
 	}
 
 	// Method to add a user ID and WebSocket object to the map
-	addUserSocket(userId, ws) {
+	addUserSocket (userId, ws) {
 		this.userSockets.set(userId, ws);
 	}
 
 	// Method to remove a user ID and WebSocket object from the map
-	removeUserSocket(userId) {
+	removeUserSocket (userId) {
 		this.userSockets.delete(userId);
 	}
 
 	// Method to retrieve the WebSocket object associated with a user ID
-	getUserSocket(userId) {
+	getUserSocket (userId) {
 		return this.userSockets.get(userId);
 	}
 }
